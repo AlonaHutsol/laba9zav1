30e629738a4fc70e32a85fa22b69c088fde66d6c
AlonaHutsol <gucolalona636@gmail.com>
2025-11-17 14:56:55 +0200

    Add files via upload

---
+++ laba9 zav1/laba9 zav1/laba9 zav1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+#include <windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int sum = 0;
+
+    for (int i = 10; i <= 100; i++) {
+        sum += i * i;
+    }
+
+    cout << "Сума квадратів від 10 до 100 = " << sum << endl;
+
+    return 0;
+}
